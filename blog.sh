eb7592d446624365f8dc5114ca7060c1e187de45
sopitz <simon.opitz@eventsos.de>
2015-03-13 12:25:41 +0100

    add docker rm for old containers, start multiple stages if necessary

--- blog.sh
+++ blog.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -tid -v ~/docker/projects/blog/simonopitz.me:/simonopitz.me --name blog localhost:5000/blog
+docker rm -f blog
+docker rm -f blog_test
+docker run -tid -v ~/docker/projects/blog/simonopitz.me:/simonopitz.me --name blog --env stage=production localhost:5000/blog
+docker run -tid -v ~/docker/projects/blog/simonopitz.me:/simonopitz.me --name blog_test --env stage=testing localhost:5000/blog
--- gateway.sh
+++ gateway.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+docker rm -f gateway
 docker run --dns $(docker inspect -f '{{.NetworkSettings.IPAddress}}' dns) --dns-search simonopitz.me -tid -p 80:80 --name gateway localhost:5000/gateway
